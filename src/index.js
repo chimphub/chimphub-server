924b3334f69df150e01b083765dfa4ccd84959ad
Fredrik Stave <fredrik.stave@e24.no>
2017-10-30 21:47:54 +0100

    refactor: added body parser

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var fs = require('fs');
 var https = require('https');
+var bodyParser = require('body-parser');
 
 var app = express();
+
 var key = fs.readFileSync('src/privkey.pem');
 var cert = fs.readFileSync('src/cert.pem');
 var ca = fs.readFileSync('src/fullchain.pem');
-
 var options = {
     key: key,
     cert: cert,
     ca: ca
 };
 
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
     res.send('WELCOME TO THE CHIMPHUB SERVER!');
 });
 
 app.post('/', function(req, res) {
-    res.send('');
+    res.send(req.body);
 });
 
 https.createServer(options, app).listen(443, function(err) {
